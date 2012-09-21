d6cf24cc69762d6fa886ade42fa84bbaafd5bbee
mahm <nishimi@sonicgarden.jp>
2012-09-21 14:54:12 +0900

    Specify class name for each store types

--- lib/nano_store/nano_store.rb
+++ lib/nano_store/nano_store.rb
@@ -1,4 +1,4 @@
-module NanoStore  
+module NanoStore
   class NanoStoreError < StandardError; end
 
   def self.store(type=:memory, path=nil)
@@ -6,23 +6,23 @@ module NanoStore
 
     case type
     when :memory
-      store = NSFNanoStore.createAndOpenStoreWithType(NSFMemoryStoreType, path:nil, error: error_ptr)
+      store = NSFNanoStore.createAndOpenStoreWithType(NSFNanoStore::NSFMemoryStoreType, path:nil, error: error_ptr)
     when :temporary, :temp
-      store = NSFNanoStore.createAndOpenStoreWithType(NSFTemporaryStoreType, path:nil, error: error_ptr)
+      store = NSFNanoStore.createAndOpenStoreWithType(NSFNanoStore::NSFTemporaryStoreType, path:nil, error: error_ptr)
     when :persistent, :file
-      store = NSFNanoStore.createAndOpenStoreWithType(NSFPersistentStoreType, path:path, error: error_ptr)
+      store = NSFNanoStore.createAndOpenStoreWithType(NSFNanoStore::NSFPersistentStoreType, path:path, error: error_ptr)
     else
       raise NanoStoreError.new("unexpected store type (#{type}), must be one of: :memory, :temporary or :persistent")
     end
 
-    raise NanoStoreError, error_ptr[0].description if error_ptr[0]    
+    raise NanoStoreError, error_ptr[0].description if error_ptr[0]
     store
   end
-  
+
   def self.shared_store
     @shared_store
   end
-  
+
   def self.shared_store=(store)
     @shared_store = store
   end
